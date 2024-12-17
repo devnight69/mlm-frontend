9782515284c288a5cc80a1b35f07e55b2422a0f1
Sahil Sharma <144793214+sahildev-ambula@users.noreply.github.com>
2024-12-18 02:01:32 +0530

    axios changes

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 // Create an Axios instance
 const axiosInstance = axios.create({
   // baseURL: "http://192.168.29.179:5000/api/",
-  baseURL: "http://localhost:5000/api/",
-  // baseURL: "https://mlm-backend-mern.vercel.app/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "https://mlm-backend-mern.vercel.app/api/",
   headers: {
     "Content-Type": "application/json",
   },
